2a1c8f1aded8f7fc0dd3f990ba45d3df025c100c
Benjamin.M <benjmonte@outlook.fr>
2025-04-15 11:06:44 +0200

    Added register form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,12 +4,14 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -30,18 +32,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
-    #[Assert\NotBlank()]
-    #[Assert\Type('string')]
-    #[Assert\Length(min: 6)]
-    //regex -> to have at elast one digit
-    #[Assert\Regex(
-        pattern: '/\d+/i', 
-    )]
-    //regex -> to have at elast one special char from the list
-    //note: list of special-char is [#?!@$%^&*-]. Adjust to suite your needs
-    #[Assert\Regex(
-        pattern: '/[#?!@$%^&*-]+/i', 
-    )]
     private ?string $password = null;
 
     #[ORM\Column(length: 255, nullable: true)]
